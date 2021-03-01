1a433be435b8ed42f05211e650495505c545e877
Alejandro Palacio <alejandrooss94@gmail.com>
2021-03-01 15:10:46 -0500

    Final manipulacion DOM

--- src/index.js
+++ src/index.js
@@ -4,3 +4,74 @@
  **/
 
 console.log('Happy hacking :)')
+/* 
+const div = document.querySelectorAll('.p-4')
+
+const arrayList = [...div]
+
+const numberRandom = () => {
+    return Math.floor(Math.random()*(122-1))+1
+}
+
+arrayList.forEach((nodo) => {
+    const img = document.createElement('img')
+    img.className = 'mx-auto'
+    img.src = `https://randomfox.ca/images/${numberRandom()}.jpg`
+    img.width = '320'
+    nodo.append(img)
+}) */
+
+import {registerImage} from './lazy'
+import {imageRender} from './lazy'
+
+const maximum = 122
+const minimum = 1
+export let imageAdd = 0
+
+const random = () => Math.floor(Math.random()* (maximum - minimum)) + minimum
+
+const createNodes = () => {
+    const container = document.createElement('div')
+    container.className = 'p-4'
+
+    const image = document.createElement('img')
+    image.className = 'mx-auto'
+    image.dataset.src = `https://randomfox.ca/images/${random()}.jpg`
+    image.width = '320'
+
+    container.appendChild(image)
+
+    return container
+}
+
+const mountNode = document.getElementById('imagenes')
+
+const addButton = document.querySelector('button')
+
+const deleteButton = document.getElementById('delete')
+
+
+const addImage = () => {
+    imageAdd++
+    console.log(`Imagen añadida: ${imageAdd}
+    Imagen renderizada: ${imageRender}`);
+
+    const newImage = createNodes();
+    mountNode.appendChild(newImage)
+    registerImage(newImage)
+}
+
+const deleteImage = () => {
+    const containers = document.querySelectorAll('div.p-4')//NodeList
+    const arrayList = [...containers]
+
+    arrayList.forEach((node) => {
+        node.remove()
+    })
+    imageAdd = 0
+}
+
+addButton.addEventListener('click', addImage)
+
+deleteButton.addEventListener('click', deleteImage)
+
---
+++ src/lazy.js
@@ -0,0 +1,30 @@
+import {imageAdd} from './index'
+export let imageRender = 0
+const isIntersecting = (entry) => {
+    return entry.isIntersecting; //true (visible nemi viewport)
+};
+
+const loadImage = (entry) => {
+    const container = entry.target //container DIV}
+    const image = container.firstChild
+    const url = image.dataset.src
+    //load image
+    image.src = url
+    imageRender++
+    if (imageAdd === 1) {
+        imageRender = 1
+    }
+    console.log(`Imagen añadida: ${imageAdd}
+    Imagen renderizada: ${imageRender}`);
+
+    observer.unobserve(container)//Deje de escuchar despues de hacer la accion
+
+}
+
+const observer = new IntersectionObserver((entries) => {
+    entries.filter(isIntersecting).forEach(loadImage)
+})
+
+export const registerImage = (image) => {
+    observer.observe(image)
+}
